2bda7de8aff5d82d24a15074198c38b0723562d3
Ryan Holden <>
2022-06-27 20:33:57 -0400

    minor refactor. remove unused code

--- Sources/HighlightingTextView/HighlightingTextView.swift
+++ Sources/HighlightingTextView/HighlightingTextView.swift
@@ -445,28 +445,27 @@ extension HighlightingTextView where Self : UITextView, Self : UIGestureRecogniz
                 return
             }
             
-            if let charNsRange = characterNSRange(at: location, adjustToNearestNonWhiteSpace: true) {
-                if let editingHighlight = highlights.first(where: {$0.nsRange.overlaps(charNsRange)}) {
-                    let isSelectedRangeInsideUserTap = selectedRange.length > 0 && selectedRange.overlaps(charNsRange)
-                    guard !isSelectedRangeInsideUserTap else {
-                        print("Tap detected while using iOS text selection. Aborting highlight menu.")
-                        return
-                    }
-                    
-                    let menu = FanOutCircleMenu()
-                    
-                    let actions = menuActionsFor(menu: menu, highlight: editingHighlight)
-                    
-                    guard !actions.isEmpty else {
-                        print("No actions provided for highlight at range: \(editingHighlight.nsRange). Aborting highlight menu.")
-                        return
-                    }
-                    
-                    menu.showOnWindow(window, anchoredView: self, center: location)
-                    
-                    menu.show(actions)
-                }
+            guard let charNsRange = characterNSRange(at: location, adjustToNearestNonWhiteSpace: true) else {return}
+            
+            guard let editingHighlight = highlights.first(where: {$0.nsRange.overlaps(charNsRange)}) else {return}
+            
+            let isSelectedRangeInsideUserTap = selectedRange.length > 0 && selectedRange.overlaps(charNsRange)
+            
+            guard !isSelectedRangeInsideUserTap else {
+                return print("Tap detected while using iOS text selection. Aborting highlight menu.")
             }
+            
+            let menu = FanOutCircleMenu()
+            
+            let actions = menuActionsFor(menu: menu, highlight: editingHighlight)
+            
+            guard !actions.isEmpty else {
+                return print("No actions provided for highlight at range: \(editingHighlight.nsRange). Aborting highlight menu.")
+            }
+            
+            menu.showOnWindow(window, anchoredView: self, center: location)
+            
+            menu.show(actions)
         default:break
         }
     }
@@ -911,75 +910,6 @@ extension UIColor {
              return light
           }
        }
-}
-
-// https://cocoacasts.com/from-hex-to-uicolor-and-back-in-swift
-extension UIColor {
-
-    // MARK: - Initialization
-
-    convenience init?(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
-        var rgb: UInt64 = 0
-
-        var r: CGFloat = 0.0
-        var g: CGFloat = 0.0
-        var b: CGFloat = 0.0
-        var a: CGFloat = 1.0
-
-        let length = hexSanitized.count
-
-        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
-
-        if length == 6 {
-            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-            b = CGFloat(rgb & 0x0000FF) / 255.0
-
-        } else if length == 8 {
-            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-            a = CGFloat(rgb & 0x000000FF) / 255.0
-
-        } else {
-            return nil
-        }
-
-        self.init(red: r, green: g, blue: b, alpha: a)
-    }
-
-    // MARK: - Computed Properties
-
-    var toHex: String? {
-        return toHex()
-    }
-
-    // MARK: - From UIColor to String
-
-    func toHex(alpha: Bool = true) -> String? {
-        guard let components = cgColor.components, components.count >= 3 else {
-            return nil
-        }
-
-        let r = Float(components[0])
-        let g = Float(components[1])
-        let b = Float(components[2])
-        var a = Float(1.0)
-
-        if components.count >= 4 {
-            a = Float(components[3])
-        }
-
-        if alpha {
-            return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
-        } else {
-            return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
-        }
-    }
-
 }
 
 extension Array {
